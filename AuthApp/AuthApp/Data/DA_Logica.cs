8d696c5d312d8e3530299e9db84d4ad67b0dc2dd
VictorLino <victor.lino@audatex.com>
2022-05-27 10:26:50 -0500

    Sample project complete for authentication with Microsoft.AspNetcore.Authentication/Authorization/Identity and using browser cookies

---
+++ AuthApp/AuthApp/Data/DA_Logica.cs
@@ -0,0 +1,23 @@
+﻿using AuthApp.Models;
+
+namespace AuthApp.Data
+{
+    public class DA_Logica
+    {
+        public List<Usuario> ListaUsuario()
+        {
+            return new List<Usuario>
+            {
+                new Usuario{ Nombre="jose", Correo="administrador@gmail.com", Clave="123", Roles=new List<string>{"Administrador"} },
+                new Usuario{ Nombre="maria", Correo="supervisor@gmail.com", Clave="123", Roles=new List<string>{"Supervisor"} },
+                new Usuario{ Nombre="juan", Correo="empleado@gmail.com", Clave="123", Roles=new List<string>{"Empleado"} },
+                new Usuario{ Nombre="oscar", Correo="superempleado@gmail.com", Clave="123", Roles=new List<string>{"Suversiro", "Empleado"} }
+            };
+        }
+
+        public Usuario ValidaUsuario(string _correo, string _clave)
+        {
+            return ListaUsuario().Where(item => item.Correo == _correo && item.Clave == _clave).FirstOrDefault();
+        }
+    }
+}
---
+++ AuthApp/AuthApp/Models/Usuario.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthApp.Models
+{
+    public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
